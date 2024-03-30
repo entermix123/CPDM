ab29ada1dcf9416ec2e036c2cba38cd5cd51071c
entermix123 <balabanovjordan@gmail.com>
2024-03-30 21:14:12 +0200

    Revert "added departments and employees models"
    This reverts commit 4862a06f0938257ea7fbb86ce9947483b7b62c2b.

--- CPDM/CPDM/departments/models.py
+++ CPDM/CPDM/departments/models.py
@@ -3,7 +3,6 @@ from django.core.validators import MinLengthValidator
 from django.db import models
 
 from CPDM.activities.models import Activity
-from CPDM.company.models import Company
 from CPDM.mixins.model_mixins import CreatedUpdatedMixin
 from CPDM.processes.models import Process
 
@@ -32,12 +31,6 @@ class Department(CreatedUpdatedMixin, models.Model):
         UserModel,
         on_delete=models.CASCADE,
         related_name='departments',
-    )
-
-    company = models.ForeignKey(
-        Company,
-        on_delete=models.CASCADE,
-        related_name='departments'
     )
 
     activities = models.ManyToManyField(
