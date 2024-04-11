2c1e0da0648a07453b45d15b71dc225e5bb60f3f
entermix123 <balabanovjordan@gmail.com>
2024-04-11 20:13:03 +0300

    added admin site for company
    Signed-off-by: entermix123 <balabanovjordan@gmail.com>

--- CPDM/CPDM/company/admin.py
+++ CPDM/CPDM/company/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from CPDM.company.models import Company
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('type', 'name', 'website', 'industry', 'owner')
+    list_filter = ('type', 'name', 'website', 'industry', 'owner')
+    search_fields = ('name', 'website', 'owner')
