de7e2e2f0e2066d4af8279bebec6e7d9b90bc9c3
entermix123 <balabanovjordan@gmail.com>
2024-04-11 20:18:11 +0300

    added admin site for departments
    Signed-off-by: entermix123 <balabanovjordan@gmail.com>

--- CPDM/CPDM/departments/admin.py
+++ CPDM/CPDM/departments/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from CPDM.departments.models import Department
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'company', 'owner')
+    list_filter = ('name', 'activities', 'company', 'owner')
+    search_fields = ('name', 'activities', 'company', 'owner')
+
