248975866ad61493a5197e4923a0179de82bb961
entermix123 <balabanovjordan@gmail.com>
2024-04-11 20:05:23 +0300

    added test for company
    Signed-off-by: entermix123 <balabanovjordan@gmail.com>

--- CPDM/CPDM/activities/admin.py
+++ CPDM/CPDM/activities/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from CPDM.activities.models import Activity
+
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+    list_filter = ('title',)
+    search_fields = ('title', 'description')
